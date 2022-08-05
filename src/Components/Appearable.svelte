65e3ed49b9186cdd57183d32cb27a0d5efac03d3
CryptoCashCashoo <83143288+CryptoCashCashoo@users.noreply.github.com>
2022-08-05 10:16:44 +0200

    :sparkles: Ratings from all positions (#2)
    * Extract appearable component
    
    * Add size to Appearable component
    
    * Add Random Offset
    
    * Cut image
    
    * Add Up and down mode for ratings
    
    * Remove unsed css
    
    * Remove unused css Again xd wtf
    
    * Add build

---
+++ src/Components/Appearable.svelte
@@ -0,0 +1,135 @@
+<script lang="ts">
+    import gsap from "gsap";
+
+    gsap.registerPlugin(ScrollTrigger);
+    export let startPercentage: number;
+    export let endPercentage: number;
+    export let mode: "U" | "R" | "L" | "D";
+    export let deltaSizePercentage: number;
+
+    const offset: number = Math.floor(Math.random() * 50);
+
+    const DEBUG_ACTIVE_ANIMATIONS: boolean = true;
+
+    let firstQuartile = (startPercentage + endPercentage) / 4;
+    let thirdQuartile = ((startPercentage + endPercentage) * 3) / 4;
+
+    let mainDiv: HTMLElement = null;
+    let YPosition: number;
+
+    function addMovement() {
+        if (DEBUG_ACTIVE_ANIMATIONS === false) return;
+        function getDeltaY(): number {
+            if (["L", "R"].includes(mode)) return 0;
+            if (mode == "D") return -window.innerHeight * deltaSizePercentage;
+            return window.innerHeight * deltaSizePercentage;
+        }
+        function getDeltaX(): number {
+            if (["U", "D"].includes(mode)) return 0;
+            if (mode == "L") return window.innerWidth * deltaSizePercentage;
+            return -window.innerWidth * deltaSizePercentage;
+        }
+        gsap.fromTo(
+            mainDiv,
+            {},
+            {
+                x: getDeltaX(),
+                y: getDeltaY(),
+                ease: "power2.out",
+                scrollTrigger: {
+                    trigger: ".scrollElement",
+                    start: startPercentage + "% top",
+                    end: firstQuartile + "% top",
+                    scrub: 1,
+                },
+            }
+        );
+
+        gsap.fromTo(
+            mainDiv,
+            {},
+            {
+                y: 0,
+                x: 0,
+                ease: "power2.out",
+                scrollTrigger: {
+                    trigger: ".scrollElement",
+                    start: thirdQuartile + "% top",
+                    end: endPercentage + "% top",
+                    scrub: 1,
+                },
+            }
+        );
+        return true;
+    }
+
+    function isActive(YPosition: number): boolean {
+        if (
+            YPosition > ((startPercentage - 5) / 100) * document.body.scrollHeight &&
+            YPosition < ((endPercentage + 5) / 100) * document.body.scrollHeight
+        ) {
+            addMovement();
+            console.log("OK");
+            return true;
+        }
+        return false;
+    }
+</script>
+
+{#if isActive(YPosition)}
+    {#if ["L", "R"].includes(mode)}
+        <div
+            bind:this={mainDiv}
+            class="Appearable columns {mode} "
+            style="
+        --deltaSize:{-deltaSizePercentage * 100 + 'vw'}; 
+        --offset:{offset + '%'}"
+        >
+            <slot />
+        </div>
+    {:else if ["U", "D"].includes(mode)}
+        <div
+            bind:this={mainDiv}
+            class="Appearable rows {mode} "
+            style="
+        --deltaSize:{-deltaSizePercentage * 100 + 'vh'}; 
+        --offset:{offset + '%'}"
+        >
+            <slot />
+        </div>
+    {/if}
+{/if}
+
+<svelte:window bind:scrollY={YPosition} />
+
+<style>
+    .Appearable {
+        position: fixed;
+        align-self: flex-start;
+        width: fit-content;
+        height: fit-content;
+    }
+    .L {
+        left: 0;
+        top: var(--offset);
+        margin-left: var(--deltaSize);
+    }
+
+    .R {
+        right: 0;
+        top: var(--offset);
+        margin-right: var(--deltaSize);
+    }
+
+    .U {
+        top: 0;
+        right: var(--offset);
+        margin-top: var(--deltaSize);
+    }
+
+    .D {
+        bottom: 0;
+        right: var(--offset);
+        margin-bottom: var(--deltaSize);
+    }
+</style>
--- src/Components/Rating.svelte
+++ src/Components/Rating.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import gsap from "gsap";
+    import Appearable from "./Appearable.svelte";
 
     import RatingCard from "./RatingCard.svelte";
     gsap.registerPlugin(ScrollTrigger);
@@ -9,88 +10,23 @@
     export let startPercentage: number;
     export let mode: "U" | "R" | "L" | "D";
     export let endPercentage: number;
-
-    const DEBUG_ACTIVE_ANIMATIONS: boolean = true;
-
-    let firstQuartile = (startPercentage + endPercentage) / 4;
-    let thirdQuartile = ((startPercentage + endPercentage) * 3) / 4;
-
-    let mainDiv: HTMLElement = null;
-    let YPosition: number;
-
-    function addMovement() {
-        if (DEBUG_ACTIVE_ANIMATIONS === false) return;
-        function getDeltaY(): number {
-            if (["L", "R"].indexOf(mode) != -1) return 0;
-            if (mode == "D") return window.innerWidth * 0.9;
-            return -window.innerWidth * 0.9;
-        }
-        function getDeltaX(): number {
-            if (["U", "D"].indexOf(mode) != -1) return 0;
-            if (mode == "L") return window.innerWidth * 0.9;
-            return -window.innerWidth * 0.9;
-        }
-        gsap.fromTo(
-            mainDiv,
-            { opacity: 1 },
-            {
-                x: getDeltaX(),
-                y: getDeltaY(),
-                ease: "power2.out",
-                scrollTrigger: {
-                    trigger: ".scrollElement",
-                    start: startPercentage + "% top",
-                    end: firstQuartile + "% top",
-                    scrub: 1,
-                },
-            }
-        );
-
-        gsap.fromTo(
-            mainDiv,
-            { opacity: 1 },
-            {
-                y: 0,
-                x: 0,
-                ease: "power2.out",
-                scrollTrigger: {
-                    trigger: ".scrollElement",
-                    start: thirdQuartile + "% top",
-                    end: endPercentage + "% top",
-                    scrub: 1,
-                },
-            }
-        );
-        return true;
-    }
-
-    function isActive(YPosition: number): boolean {
-        if (
-            YPosition > ((startPercentage - 5) / 100) * document.body.scrollHeight &&
-            YPosition < ((endPercentage + 5) / 100) * document.body.scrollHeight
-        ) {
-            addMovement();
-            return true;
-        }
-        return false;
-    }
 </script>
 
-{#if isActive(YPosition)}
+<Appearable {startPercentage} {endPercentage} {mode} deltaSizePercentage={0.9}>
     {#if mode === "L"}
-        <div bind:this={mainDiv} class="{mode} columns ">
-            <img class="TcelaImage" src="./Images/TcelaDibus/patotata.png" alt="Tcela speaking" />
-            <RatingCard {name} {stars} {review} />
-        </div>
+        <img class="TcelaImage" src="./Images/TcelaDibus/patotata.png" alt="Tcela speaking" />
+        <RatingCard {name} {stars} {review} />
     {:else if mode === "R"}
-        <div bind:this={mainDiv} class="columns  {mode}">
-            <RatingCard {name} {stars} {review} />
-            <img class="TcelaImage flipX" src="./Images/TcelaDibus/patotata.png" alt="Tcela speaking" />
-        </div>
+        <RatingCard {name} {stars} {review} />
+        <img class="TcelaImage flipX" src="./Images/TcelaDibus/patotata.png" alt="Tcela speaking" />
+    {:else if mode === "U"}
+        <img class="TcelaImage rotate90" src="./Images/TcelaDibus/patotataV.png" alt="Tcela speaking" />
+        <RatingCard {name} {stars} {review} />
+    {:else if mode === "D"}
+        <RatingCard {name} {stars} {review} />
+        <img class="TcelaImage flipY" src="./Images/TcelaDibus/patotataV.png" alt="Tcela speaking" />
     {/if}
-{/if}
-
-<svelte:window bind:scrollY={YPosition} />
+</Appearable>
 
 <style>
     .TcelaImage {
@@ -100,29 +36,8 @@
     .flipX {
         transform: scaleX(-1);
     }
-    .R {
-        right: 0;
-        position: fixed;
-        top: 5%;
-        align-self: flex-start;
-        width: fit-content;
-        margin-right: calc(-45vw * 2);
-    }
 
-    .L {
-        left: 0;
-        position: fixed;
-        top: 5%;
-        align-self: flex-start;
-        margin-left: calc(-45vw * 2);
-        width: fit-content;
-    }
-
-    .U {
-        top: 0;
-    }
-
-    .D {
-        bottom: 0;
+    .flipY {
+        transform: scaleY(-1);
     }
 </style>

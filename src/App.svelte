177b873b0cbf6c792edad72f5fd57d436173fe82
cryptocashcashoo <cryptocashcashoo@gmail.com>
2022-08-05 08:10:57 +0200

    Add accents

--- src/App.svelte
+++ src/App.svelte
@@ -15,7 +15,7 @@
     <Rating
         name="CarlosMushuTV"
         stars={1}
-        review="Intento aprender de su contenido pero me es imposible por que me hornea demasiado verlo "
+        review="Intento aprender de su contenido pero me es imposible por que me hornea demasiado verlo."
         startPercentage={10}
         endPercentage={20}
         mode="L"
@@ -23,7 +23,7 @@
     <Rating
         name="WilmiRosa"
         stars={5}
-        review="Mejor programador de UwU!"
+        review="¡Mejor programador de UwU!"
         startPercentage={40}
         endPercentage={50}
         mode="R"
@@ -31,7 +31,7 @@
     <Rating
         name="WayraisBleu"
         stars={5}
-        review="Felizmente me encontre con este programador y me pareció una persona muy amable y atenta, por lo que casé con el"
+        review="Felizmente me encontré con este programador y me pareció una persona muy amable y atenta, por lo que casé con él."
         startPercentage={70}
         endPercentage={80}
         mode="L"

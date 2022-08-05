d6fd86e9a6fd4db58709f144227de9f77cef78b6
cryptocashcashoo <cryptocashcashoo@gmail.com>
2022-08-05 10:12:29 +0200

    Remove unsed css

--- src/Components/RatingCard.svelte
+++ src/Components/RatingCard.svelte
@@ -44,7 +44,7 @@
         height: 6vh;
         width: 100%;
         background-image: url("/Images/Lines/hr.png");
-        background-size: cover; /* <------ */
+        background-size: cover;
         background-repeat: no-repeat;
         background-position: center center;
         background-size: 100% 100%;
@@ -52,8 +52,7 @@
     .full-star {
         display: inline-block;
         position: relative;
-        /* content: "\2605"; */
-        background-size: cover; /* <------ */
+        background-size: cover;
         background-repeat: no-repeat;
         background-position: center center;
         background-size: 100% 100%;

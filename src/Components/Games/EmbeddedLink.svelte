3377ab41519055c6a1488930467fbf64fd8bfea7
CryptoCashCashoo <83143288+CryptoCashCashoo@users.noreply.github.com>
2022-08-08 18:35:14 +0200

    :pencil: :video_game: :sparkles: Add games (#5)
    * Add youtube viewer, gif, and mp4 webgm players
    
    * remove log and remove audio player in gif
    
    * Add tentacle ship video and Despertar de Arari
    
    * embedding tiktok video
    
    * todo remainder xD omg
    
    * Add tiktok embed js on indexes
    
    * Add more games and active tiktok iframe
    
    * Add build

--- src/Components/Games/EmbeddedLink.svelte
+++ src/Components/Games/EmbeddedLink.svelte
@@ -1,32 +1,88 @@
 <script lang="ts">
-    type EmbeddedType = "png" | "gif" | "mp4";
+    import Youtube from "svelte-youtube";
+    type EmbeddedType = "png" | "gif" | "mp4" | "webm" | "youtube" | "tiktok";
 
     export let embeddedLink: string;
     export let aspectRatio: "expand" | "preserveRatio" = "preserveRatio";
-    // TODO: Parse link to deduce type: png / gif / mp4 etc etc etc etc
 
+    let videoID: string = null;
     let embeddedType: EmbeddedType = getEmbeddedType(embeddedLink);
 
     function getEmbeddedType(link: string): EmbeddedType {
+        if (embeddedLink.startsWith("https://www.youtube.com") || embeddedLink.startsWith("www.youtube.com")) {
+            videoID = getYoutubeId(link);
+            if (videoID === null) {
+                console.error("TYPE " + link + " Isn't recognized.");
+                return;
+            }
+            return "youtube";
+        }
+        if (embeddedLink.startsWith("https://www.tiktok.com") || embeddedLink.startsWith("www.tiktok.com")) {
+            videoID = embeddedLink.split("/")[embeddedLink.split("/").length - 1];
+            return "tiktok";
+        }
+
         if (embeddedLink.endsWith(".png")) return "png";
         else if (embeddedLink.endsWith(".gif")) return "gif";
         else if (embeddedLink.endsWith(".mp4")) return "mp4";
+        else if (embeddedLink.endsWith(".webm")) return "webm";
+
         console.error("TYPE " + link + " Isn't recognized.");
+    }
+
+    function getYoutubeId(link: string): string | null {
+        var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+        var match = link.match(regExp);
+        return match && match[7].length == 11 ? match[7] : null;
+    }
+
+    const YoutubeOptions = {
+        width: "100%",
+        height: "100%",
+        playerVars: {
+            rel: 0,
+            modestbranding: 1,
+            autoplay: 1,
+        },
+    };
+
+    function onYTVideoEnd(event) {
+        event.detail.target.playVideo();
     }
 </script>
 
 {#if embeddedType === "png"}
     <img src={embeddedLink} alt="Embedded link" class={aspectRatio} />
 {:else if embeddedType === "gif"}
-    <div />
-{:else if embeddedType === "mp4"}
-    <div />
+    <div class="expand ">
+        <img src={embeddedLink} alt="Embedded link" class={aspectRatio} />
+    </div>
+{:else if embeddedType === "mp4" || embeddedType === "webm"}
+    <div class="expand blackBackground">
+        <video src={embeddedLink} class={aspectRatio} autoplay loop controls><track kind="captions" /></video>
+    </div>
+{:else if embeddedType === "youtube"}
+    <Youtube videoId={videoID} options={YoutubeOptions} class="YOUTUBE_VIDEO_CONTAINER" on:end={onYTVideoEnd} />
+{:else if embeddedType === "tiktok"}
+    <!-- <blockquote
+        class="tiktok-embed"
+        cite="https://www.tiktok.com/@tceladev/video/{videoID}"
+        data-video-id={videoID}
+        style="width: 500px; height: 500px; scroll:hidden;"
+    >
+        <section />
+    </blockquote> -->
+    <iframe title="tiktok video" src="https://www.tiktok.com/embed/v2/{videoID}" class={aspectRatio} />
 {/if}
 
 <style>
+    .blackBackground {
+        background-color: black;
+    }
     .expand {
         width: 100%;
         height: 100%;
+        display: block;
     }
 
     .preserveRatio {
--- src/gameInfo.ts
+++ src/gameInfo.ts
@@ -8,36 +8,81 @@ export interface GameInfo{
 
 }
 
-
-
 const Games: GameInfo[] =
-[{title:"Tentacles Ship",
-description: `MICHE JAM GAME MADE WITH UNITY.
-POPULAR GAME FOR EVERYONE. 
-2 DAYS OF DEVELEPMENT AND A LOT.
-HOP YOU LIKE IT. IF NOT GIVE ME A FEEDBACK. THANKS 
-
-MICHE JAM GAME MADE WITH UNITY.
-POPULAR GAME FOR EVERYONE. 
-2 DAYS OF DEVELEPMENT AND A LOT.
-HOP YOU LIKE IT. IF NOT GIVE ME A FEEDBACK. THANKS 
-
-
-`,
-link: ["https://tcela.itch.io/tentacles-ship"],
-embeddedLink:"https://user-images.githubusercontent.com/83143288/183268397-f2cbc797-ff76-44fd-8aca-866e16d18ede.png",
-embeddedAspect :"expand"
+[    
+{
+    title:"Horror game (WIP)",
+    description: `Under development (TODO TALK WITH MUSHU PILOT)`,
+    link: ["https://www.twitch.tv/tcela"],
+    embeddedLink:"https://www.tiktok.com/embed/v2/7114078078732750085",
+    embeddedAspect :"expand"
+}, 
+{
+    title:"Procedural Dungeon",
+    description: `A basic way to develop a random and procedural 3D Dungeon with customized prefabs (models). Available on github!`,
+    link: ["https://github.com/tenecela19/Dungeon_Procedural"],
+    embeddedLink:"https://www.tiktok.com/embed/v2/7106320568089070854",
+    embeddedAspect :"expand"   
+},    
+{
+    title:"Spatial Game",
+    description: `The famous spatial game.`,
+    link: ["https://www.twitch.tv/tcela"],
+    embeddedLink:"https://www.tiktok.com/embed/v2/7055194299008339205",
+    embeddedAspect :"expand"
 },
-
-{title:"Tentacles Ship",
-description: `MICHE JAM GAME MADE WITH UNITY.
-POPULAR GAME FOR EVERYONE. 
-2 DAYS OF DEVELEPMENT AND A LOT.
-HOP YOU LIKE IT. IF NOT GIVE ME A FEEDBACK. THANKS 
-`,
-link: ["https://tcela.itch.io/tentacles-ship"],
-embeddedLink:"https://user-images.githubusercontent.com/83143288/183268397-f2cbc797-ff76-44fd-8aca-866e16d18ede.png",
-embeddedAspect :"preserveRatio"
+{
+    title:"Tcela and Jayrumi dance",
+    description: `Yeah, this is not a game, but.. well, it's Tcela and Jayrumi dancing!`,
+    link: ["https://www.twitch.tv/tcela"],
+    embeddedLink:"https://user-images.githubusercontent.com/83143288/183411849-2297da5e-f505-4494-a920-7dd757433685.mp4",
+    embeddedAspect :"expand"
+},
+{
+    title:"Tentacles Ship",
+    description: `Miche jam game made with Unity.
+    Popular game for everyone.
+    2 fays of development and a lot.
+    Hope you like it. If not, give me feedback. Thanks!`,
+    link: ["https://tcela.itch.io/tentacles-ship"],
+    embeddedLink:"/videos/TentaclesShip.mp4",
+    embeddedAspect :"expand"
+},
+{
+    title:"Project (Paraminia)",
+    description: `Experiment # 7 wake up.
+    The military base is being destroyed by a failure in the system. 
+    One of the most promising experiments of the new century. 
+    Travel back in time, it has corrupted and freeing all the experiments saved for years. 
+    Destroying everything in its path and destroying the building. 
+    Creating a  breakdown in space time. We become the only option to be able to reverse what it may be the end of the world.`,
+    link: ["https://tcela.itch.io/paraminia"],
+    embeddedLink:"https://www.youtube.com/watch?v=xoG298XpNLM",
+    embeddedAspect :"preserveRatio"
+},
+{
+    title:"UwU",
+    description: `UwU`,
+    link: ["https://www.twitch.tv/tcela"],
+    embeddedLink:"https://www.icegif.com/wp-content/uploads/2022/02/icegif-924.gif",
+    embeddedAspect :"expand"
+},
+{
+    title:"The cake game?",
+    description: `Sweetly made for you `,
+    link: ["https://www.twitch.tv/tcela"],
+    embeddedLink:"https://www.tiktok.com/embed/v2/6964181556793445637",
+    embeddedAspect :"expand"
+},
+{
+    title:"El despertar de Arari",
+    description: `El reposo de Arari fue interrumpido cuando un grupo de traficantes de fósiles extrajeron ilícitamente sus restos y los llevaron a un museo lejano. Ahora necesita tu ayuda para recuperar y restaurar la mitad de su cráneo.
+    Explora distintos escenarios, presta atención a los objetos que te rodean y resuelve acertijos para que Arari pueda descansar en paz y para repatriar otros fósiles extraídos y vendidos ilegalmente.
+    Tu misión será arriesgada, pero Arari estará junto a ti para guiarte.
+    `,
+    link: ["https://faramilner.itch.io/el-despertar-de-arari"],
+    embeddedLink:"https://www.youtube.com/watch?v=EQQGTiAKNUk&t",
+    embeddedAspect :"preserveRatio"
 },
 
 ]

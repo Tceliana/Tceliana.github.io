c76e5ae7c04d9e2bb1dfdfcc80592897e3d644f8
cryptocashcashoo <cryptocashcashoo@gmail.com>
2022-08-08 03:42:13 +0200

    Split columns in two

--- src/Components/Games/GameView.svelte
+++ src/Components/Games/GameView.svelte
@@ -12,13 +12,20 @@
 
 <div class="gameView">
     <SvgScrollPath {flipX} {startAtPixelY} {endAtPixelY}>
-        {#if flipX}
-            <img src={gameInfo.embeddedLink} alt="dsadsa" style="width:50vw;" />
-            <GameDescription {gameInfo} style="width:50vw;" />
-        {:else}
-            <GameDescription {gameInfo} style="width:50vw;" />
-            <img src={gameInfo.embeddedLink} alt="dsadsa" style="width:50vw;" />
-        {/if}
+        <div class="midScreen">
+            {#if flipX}
+                <img src={gameInfo.embeddedLink} alt="dsadsa" style="width:50vw;" />
+            {:else}
+                <GameDescription {gameInfo} style="width:50vw;" />
+            {/if}
+        </div>
+        <div class="midScreen">
+            {#if flipX}
+                <GameDescription {gameInfo} style="width:50vw;" />
+            {:else}
+                <img src={gameInfo.embeddedLink} alt="dsadsa" style="width:50vw;" />
+            {/if}
+        </div>
     </SvgScrollPath>
 </div>
 
@@ -27,5 +34,10 @@
         position: fixed;
         top: 0;
         z-index: -1;
+    }
+
+    .midScreen {
+        width: 50vw;
+        height: 100vh;
     }
 </style>

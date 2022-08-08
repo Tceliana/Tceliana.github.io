880cdc85308eba6c82917faca4f2f5934ef2f405
cryptocashcashoo <cryptocashcashoo@gmail.com>
2022-08-08 11:27:13 +0200

    Game Description style

--- src/Components/Games/GameDescription.svelte
+++ src/Components/Games/GameDescription.svelte
@@ -5,16 +5,36 @@
 </script>
 
 <div class="gameView" style={stylish}>
-    <a href={gameInfo.link[0]}>{gameInfo.title}</a>
+    <h1>
+        <a href={gameInfo.link[0]}>{gameInfo.title}</a>
+    </h1>
     <div>{gameInfo.description}</div>
 </div>
 
 <style>
+    h1 {
+        position: relative;
+        margin: 0 0 1vh 0;
+        color: var(--COLOR_SECONDARY);
+        margin-left: auto;
+        margin-right: auto;
+        width: fit-content;
+        font-weight: lighter;
+        text-decoration: underline;
+        text-decoration-thickness: 0.3vh;
+    }
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+    h1:hover {
+        font-weight: bolder;
+    }
     .gameView {
         box-sizing: border-box;
         margin-left: 1vw;
-        padding: 5vh 1vw;
-        filter: drop-shadow(0px 2px 3px rgba(0, 0, 255, 0.5));
+        padding: 1vh 1vw 2vh 1vw;
+        filter: drop-shadow(0px 2px 3px rgba(0, 0, 255, 0.3));
         background-color: rgb(255 255 255 /10%);
         border-radius: 0.5vh;
         box-shadow: 0 1px 9px 0 rgb(0 0 0 / 20%);

e56d302993bf4bd3a90da327856bb6550f4ab481
cryptocashcashoo <cryptocashcashoo@gmail.com>
2022-08-08 09:37:44 +0200

    Games height depends on games count ^^

--- src/Pages/Games.svelte
+++ src/Pages/Games.svelte
@@ -1,33 +1,36 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+
     import GameView from "../Components/Games/GameView.svelte";
     import Games from "../gameInfo";
-    const max_scrollbar_height: number = document.body.scrollHeight - window.innerHeight;
+    const pageHeight: number = (Games.length + 1) * 1.5;
     const lengthOfOneGameViewPer = 1 / Games.length;
-
-    console.log(Games);
+    let max_scrollbar_height: number = window.innerHeight * pageHeight;
 </script>
 
-<div class="background" />
-<div class="rows">
-    <h1 style="font-family: earthsmightiestbold; color: var(--COLOR_PRIMARY);">GAMES</h1>
-    {#each Games as game, index}
-        {#if index % 2 == 0}
-            <GameView
-                startAtPixelY={index * lengthOfOneGameViewPer * max_scrollbar_height}
-                endAtPixelY={(index+1) * lengthOfOneGameViewPer  * max_scrollbar_height}
-                gameInfo={game}
-            />
-        {:else}
-            <GameView
-                flipX
-                startAtPixelY={index * lengthOfOneGameViewPer * max_scrollbar_height}
-                endAtPixelY={(index+1) * lengthOfOneGameViewPer * max_scrollbar_height}
-                gameInfo={game}
-            />
-        {/if}
-    {/each}
+<div style="min-height: {(pageHeight + 1.5) * 100}vh;">
+    <div class="background" />
+    <div class="rows">
+        <h1 style="font-family: earthsmightiestbold; color: var(--COLOR_PRIMARY); ">GAMES</h1>
+        {#each Games as game, index}
+            {#if index % 2 == 0}
+                <GameView
+                    startAtPixelY={index * lengthOfOneGameViewPer * max_scrollbar_height}
+                    endAtPixelY={(index + 1) * lengthOfOneGameViewPer * max_scrollbar_height}
+                    gameInfo={game}
+                />
+            {:else}
+                <GameView
+                    flipX
+                    startAtPixelY={index * lengthOfOneGameViewPer * max_scrollbar_height}
+                    endAtPixelY={(index + 1) * lengthOfOneGameViewPer * max_scrollbar_height}
+                    gameInfo={game}
+                />
+            {/if}
+        {/each}
+    </div>
+    <img class="footer" src="./Images/TcelaDibus/beach.png" alt="Tcela looking at infinitum xD" />
 </div>
-<img class="footer" src="./Images/TcelaDibus/beach.png" alt="Tcela looking at infinitum xD" />
 
 <style>
     .footer {

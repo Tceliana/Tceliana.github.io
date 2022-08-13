d297e934a9e931945728631018dc7c9240d2b348
cryptocashcashoo <cryptocashcashoo@gmail.com>
2022-08-13 23:52:21 +0200

    Remove unused Social Networks

--- src/Components/Header/SocialNetworks.svelte
+++ src/Components/Header/SocialNetworks.svelte
@@ -5,22 +5,7 @@
 
 <div class="container">
     <div class="pointer"></div>
-    <div class="columns bubble">
-        <div class="icon icon--twitter">
-          <a href="#">
-            <span class="icon__name">Twitter</span>
-          </a>
-        </div>
-        <div class="icon icon--linkedin">
-          <a href="#">
-            <span class="icon__name">LinkedIn</span>
-          </a>
-        </div>
-        <div class="icon icon--angellist">
-          <a href="#">
-            <span class="icon__name">Angellist</span>
-          </a>
-        </div>
+    <div class="columns bubble flexContainer">
         <div class="icon icon--instagram">
           <a href="#">
             <span class="icon__name">Instagram</span>
@@ -36,24 +21,9 @@
             <span class="icon__name">Spotify</span>
           </a>
         </div>
-        <div class="icon icon--soundcloud">
-          <a href="#">
-            <span class="icon__name">Soundcloud</span>
-          </a>
-        </div>
-        <div class="icon icon--songkick">
-          <a href="#">
-            <span class="icon__name">Songkick</span>
-          </a>
-        </div>
         <div class="icon icon--github">
           <a href="#">
             <span class="icon__name">GitHub</span>
-          </a>
-        </div>
-          <div class="icon icon--codepen">
-          <a href="#">
-            <span class="icon__name">Codepen</span>
           </a>
         </div>
     </div>   
@@ -70,26 +40,25 @@
     border-radius: 12px 0 0 0;
     position: absolute;
     margin-left:2.25vw;
-
 }
 
 .bubble {
-	height: 6vw;
+    height: 6vw;
     width:fit-content;
-	background: var(--COLOR_BUBBLE_BACKGROUND);
-	margin: 1.5vw 0px;
-	border-radius: 20px;
-	text-align: center;
+    background: var(--COLOR_BUBBLE_BACKGROUND);
+    margin: 1.5vw 0px;
+    border-radius: 20px;
+    text-align: center;
     position:absolute;
     right:2vw;
     padding:1vw;
     box-sizing: border-box;
-    
 }
 
 .flexContainer
 {
     justify-content: space-around;
+    gap: 0.5vw;
 }
 
 .icon {
@@ -142,20 +111,6 @@
   filter: blur(0);
 }
 
-.icon--codepen a {
-  background-color: #111;
-  background-image: url("https://benhodgson.net/img/social/codepen.svg");
-}
-
-.icon--twitter a {
-  background-color: #00aced;
-  background-image: url("https://benhodgson.net/img/social/twitter.svg");
-}
-
-.icon--linkedin a {
-  background-color: #007fb1;
-  background-image: url("https://benhodgson.net/img/social/linkedin.svg");
-}
 
 .icon--instagram a {
   background-color: #405de6;
@@ -170,21 +125,6 @@
 .icon--spotify a {
   background-color: #80b719;
   background-image: url("https://benhodgson.net/img/social/spotify.svg");
-}
-
-.icon--soundcloud a {
-  background-color: #f60;
-  background-image: url("https://benhodgson.net/img/social/soundcloud.svg");
-}
-
-.icon--songkick a {
-  background-color: #f80046;
-  background-image: url("https://benhodgson.net/img/social/songkick.svg");
-}
-
-.icon--angellist a {
-  background-color: #eee;
-  background-image: url("https://benhodgson.net/img/social/angellist.svg");
 }
 
 .icon--github a {

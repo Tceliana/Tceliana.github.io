a8c45430865c4d578371c5032f8d1e40f84a542f
CryptoCashCashoo <83143288+CryptoCashCashoo@users.noreply.github.com>
2022-08-16 13:33:38 +0200

    :balloon: Social net style (#11)
    * Add wayra resources
    
    * space header
    
    * Extract component SocialNetwork Link
    
    * Add images
    
    * Add balloons
    
    * byebye social networks
    
    * Add balloon animation

---
+++ src/Components/Header/SocialNetLink.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+
+  export let link :string;
+  export let imageUrl :string;
+  export let backgroundColor :string;
+
+</script>
+
+<div class="icon">
+  <a target="_blank" href="{link}" style="background-color:{backgroundColor}; background-image:url({imageUrl})">
+    <img class="heart" src="/Images/heart.svg" alt="heart SVG <3"/> 
+  </a>
+</div>
+
+<style>
+
+.icon {
+  float:      left;
+  position:   relative;
+  width:      7vh;
+  height:     7vh;
+}
+
+.icon:last-of-type {
+  margin-right: 0;
+}
+
+.icon a {
+  display:              block;
+  left:                 0;
+  width:                100%;
+  height:               100%;
+  border:               0;
+  border-radius:        10px;
+  background-color:     #fff;
+  background-size:      100%;
+  background-repeat:    no-repeat;
+  background-position:  center center;
+  transition:           all 0.2s ease-out;
+  filter:               drop-shadow(4px 5px 2px rgba(0, 0, 0, 0.4));
+}
+
+.icon a:hover {
+  background-color: #222;
+  background-size:  0%;
+  transition:       all 0.2s ease-out;
+}
+
+img {
+  width:          70%;
+  color:        #fff;
+  position:       absolute;
+  left:           0;
+  right:          0;
+  top:            0;
+  bottom:         0;
+  margin:         auto;
+  transition:     all 0.3s ease-out;
+  opacity:        0;
+  filter:         blur(25px);
+  will-change:    transform;
+}
+
+.icon:hover img {
+  transition: all 0.3s ease-out;
+  opacity:    1;
+  filter:     blur(0);
+}
+
+</style>

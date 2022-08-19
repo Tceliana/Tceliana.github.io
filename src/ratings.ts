d49be92e7efa670a7595aa9f051f75ab8f532b95
cryptocashcashoo <cryptocashcashoo@gmail.com>
2022-08-19 20:30:55 +0200

    Add raw ratings

--- src/ratings.ts
+++ src/ratings.ts
@@ -1,28 +1,47 @@
-export interface RatingInfo{
-    name : string;
-    review: string;
-    stars: number;
+export interface RatingInfo
+{
+	name: string;
+	review: string;
+	stars: number;
 }
 
 export const AllRatings: RatingInfo[] = [
-{
-	name:"CarlosMushuTV",
-	stars:1,
-	review:"Intento aprender de su contenido pero me es imposible por que me hornea demasiado verlo.",
-},
-{
-	name:"WilmiRosa",
-	stars:5,
-	review:"¡Mejor programador de UwU!",
-},
-{
-	name:"WayraisBleu",
-	stars:5,
-	review:"Felizmente me encontré con este programador y me pareció una persona muy amable y atenta, por lo que casé con él.",
-},
-{
-	name:"Chicken",
-	stars:5,
-	review:"Felizmente me encontré con este programador y me pareció una persona muy amable y atenta, por lo que casé con él.",
-},
+	{
+		name: "Un amigo muy extraño",
+		stars: 5,
+		review: `Umm no sé cómo empezar esto la verdad, quizás desde el comienzo ¿Sabías que, literalmente, el primer stream que vi y que realmente me intereso, fue el tuyo? Era muy divertido y muy sincero principalmente eso, además de ser muy auténtico, y sentir afinidad contigo (no homo) afinidad de hermano, no tengo ni idea si sentiste lo mismo conmigo, pero te quiero mucho hermano, nos hemos contado mutuamente cosas personales y problemas con la intención de que el otro nos escuche y que además nos diera consejos o una orientación, fuiste quien… sin saberlo me motivo hacer stream, eres alguien a quien sin conocer en persona, siento que disfrutaríamos mucho saliendo y jugando en personas, eres una persona increíble nunca cambies mi hermano, eres una persona muy auténtica y te felicito como has superado, todas las adversidades a las que has enfrentado en EEUU, sé que no ha sido fácil, mereces un feliz cumpleaños increíble, tal como la persona que eres …
+			Posdata: gracias por ayudarme el día que de verdad necesitaba ayuda. 
+		
+			Como decías en tus primeros directos (Corazón Koreano para ti)`,
+	},
+	{
+		name: "WayraisBleu",
+		stars: 5,
+		review: "Felizmente me encontré con este programador y me pareció una persona muy amable y atenta, por lo que casé con él.",
+	},
+	{
+		name: "CarlosMushuTV",
+		stars: 1,
+		review: "Intento aprender de su contenido pero me es imposible por que me hornea demasiado verlo.",
+	},
+	{
+		name: "WilmiRosa",
+		stars: 5,
+		review: "¡Mejor programador de UwU!",
+	},
+	{
+		name: "PodcastMataró",
+		stars: 5,
+		review: "Tata tenía tanto talento, que tecleo'sta obtener a twayra, y también tomó a taro en su aposento. Taro tuvo una vida contento, junto a tblue tuvieron un entierro, y con pato en el centro.",
+	},
+	{
+		name: "Cornelio",
+		stars: 5,
+		review: "Él si era",
+	},
+	{
+		name: "Chicken",
+		stars: 5,
+		review: "Felizmente me encontré con este programador y me pareció una persona muy amable y atenta, por lo que casé con él.",
+	},
 ]

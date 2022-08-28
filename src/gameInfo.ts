ef1c50bdf8b6019167d4f6f235fdc4ce3ff540e5
cryptocashcashoo <cryptocashcashoo@gmail.com>
2022-08-28 13:28:03 +0200

    simplify gameinfo def

--- src/gameInfo.ts
+++ src/gameInfo.ts
@@ -2,7 +2,7 @@
 export interface GameInfo{
     title: string;
     description: string;
-    link: [string];
+    link: string;
 	embeddedLink: string; //png,gif,mp4
     embeddedAspect: "expand" | "preserveRatio";
 
@@ -13,7 +13,7 @@ const Games: GameInfo[] =
 {
     title:"Horror game (WIP)",
     description: `Under development.`,
-    link: ["https://www.twitch.tv/tcela"],
+    link: "https://www.twitch.tv/tcela",
     embeddedLink:"https://user-images.githubusercontent.com/83143288/184445754-6798ff82-6ec8-4e25-b4a5-d7751938b0d2.mp4",
     embeddedAspect :"expand"
 }, 
@@ -21,39 +21,38 @@ const Games: GameInfo[] =
     title:"Super Dungeon",
     description: `Survival game where agility and strategy is the number one key to<br/>
     Get the victory, fight the hordes and advance all levels to the end.`,
-    link: ["https://play.google.com/store/apps/details?id=com.TcelaGames.SuperDungeon"],
+    link: "https://play.google.com/store/apps/details?id=com.TcelaGames.SuperDungeon",
     embeddedLink:"https://www.youtube.com/watch?v=SYMWNhR8hdg",
-    embeddedAspect :"expand"
+    embeddedAspect :"preserveRatio"
 }, 
 {
     title:"Free Fall",
     description: `You will have to demonstrate your dodging skills, where the ball will have to dodge the walls until it reaches the goal. You can you can cross on the sides, or under them, all to your imagination.<br/>
     A game of strategy and speed.<br/>
     The higher the level, the more difficult it will be to reach the goal. Are you able to do it?`,
-    link: ["https://play.google.com/store/apps/details?id=com.Tcela.FreeFall"],
+    link: "https://play.google.com/store/apps/details?id=com.Tcela.FreeFall",
     embeddedLink:"https://user-images.githubusercontent.com/83143288/187065006-f9c88b1f-8fc5-4bbc-8330-30f88e38712b.png",
     embeddedAspect :"expand"
 }, 
-
+{
+    title:"Tcela and Jayrumi dance",
+    description: `Yeah, this is not a game, but.. well, it's Tcela and Jayrumi dancing!`,
+    link: "https://www.twitch.tv/tcela",
+    embeddedLink:"https://user-images.githubusercontent.com/83143288/183411849-2297da5e-f505-4494-a920-7dd757433685.mp4",
+    embeddedAspect :"expand"
+},
 {
     title:"Procedural Dungeon",
     description: `A basic way to develop a random and procedural 3D Dungeon with customized prefabs (models).<br/><br/> Available on github!`,
-    link: ["https://github.com/tenecela19/Dungeon_Procedural"],
+    link: "https://github.com/tenecela19/Dungeon_Procedural",
     embeddedLink:"https://user-images.githubusercontent.com/83143288/184445747-ec89b0ea-8503-4dff-aac9-378c0c91ebf6.mp4",
     embeddedAspect :"expand"   
 },    
 {
     title:"Spatial Game",
     description: `The famous spatial game.`,
-    link: ["https://www.twitch.tv/tcela"],
+    link: "https://www.twitch.tv/tcela",
     embeddedLink:"https://user-images.githubusercontent.com/83143288/184445736-e8fd52fe-0961-4d05-aecc-c6a2d8e79aaf.mp4",
-    embeddedAspect :"expand"
-},
-{
-    title:"Tcela and Jayrumi dance",
-    description: `Yeah, this is not a game, but.. well, it's Tcela and Jayrumi dancing!`,
-    link: ["https://www.twitch.tv/tcela"],
-    embeddedLink:"https://user-images.githubusercontent.com/83143288/183411849-2297da5e-f505-4494-a920-7dd757433685.mp4",
     embeddedAspect :"expand"
 },
 {
@@ -62,7 +61,7 @@ const Games: GameInfo[] =
     Popular game for everyone.<br/>
     2 fays of development and a lot.<br/>
     Hope you like it. If not, give me feedback. Thanks!`,
-    link: ["https://tcela.itch.io/tentacles-ship"],
+    link: "https://tcela.itch.io/tentacles-ship",
     embeddedLink:"/videos/TentaclesShip.mp4",
     embeddedAspect :"expand"
 },
@@ -73,21 +72,21 @@ const Games: GameInfo[] =
     Travel back in time, it has corrupted and freeing all the experiments saved for years. <br/>
     Destroying everything in its path and destroying the building. <br/>
     Creating a  breakdown in space time. We become the only option to be able to reverse what it may be the end of the world.`,
-    link: ["https://tcela.itch.io/paraminia"],
+    link: "https://tcela.itch.io/paraminia",
     embeddedLink:"https://www.youtube.com/watch?v=xoG298XpNLM",
     embeddedAspect :"preserveRatio"
 },
 {
     title:"UwU",
     description: `UwU`,
-    link: ["https://www.twitch.tv/tcela"],
+    link: "https://www.twitch.tv/tcela",
     embeddedLink:"https://www.icegif.com/wp-content/uploads/2022/02/icegif-924.gif",
     embeddedAspect :"expand"
 },
 {
     title:"The cake game?",
     description: `Sweetly made for you `,
-    link: ["https://www.twitch.tv/tcela"],
+    link: "https://www.twitch.tv/tcela",
     embeddedLink:"https://user-images.githubusercontent.com/83143288/184445812-c63af07f-c7aa-4628-837a-ec9548da0596.mp4",
     embeddedAspect :"expand"
 },
@@ -96,7 +95,7 @@ const Games: GameInfo[] =
     description: `El reposo de Arari fue interrumpido cuando un grupo de traficantes de fósiles extrajeron ilícitamente sus restos y los llevaron a un museo lejano. Ahora necesita tu ayuda para recuperar y restaurar la mitad de su cráneo.
     Explora distintos escenarios, presta atención a los objetos que te rodean y resuelve acertijos para que Arari pueda descansar en paz y para repatriar otros fósiles extraídos y vendidos ilegalmente.
     Tu misión será arriesgada, pero Arari estará junto a ti para guiarte.`,
-    link: ["https://faramilner.itch.io/el-despertar-de-arari"],
+    link: "https://faramilner.itch.io/el-despertar-de-arari",
     embeddedLink:"https://www.youtube.com/watch?v=EQQGTiAKNUk&t",
     embeddedAspect :"preserveRatio"
 },

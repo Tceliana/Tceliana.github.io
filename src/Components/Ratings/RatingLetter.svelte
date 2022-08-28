cc2fe11ae82ec11fac3df517621974deb03c101d
cryptocashcashoo <cryptocashcashoo@gmail.com>
2022-08-28 10:46:47 +0200

    fix ratting letter not found

--- src/Components/Ratings/RatingLetter.svelte
+++ src/Components/Ratings/RatingLetter.svelte
@@ -23,7 +23,8 @@
         let velocity = 0.0007;
 
         function AutoScroll() {           
-
+            if(textReview === null)
+                return;
             textReview.scrollTop= Math.floor(scrollPercentage * textReview.scrollHeight);
             backgroundScroll = textReview.scrollTop -0.248* window.innerWidth;
 

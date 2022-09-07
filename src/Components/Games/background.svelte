dba73978f5baf372a7df6660475c6b9c52948b7b
cryptocashcashoo <cryptocashcashoo@gmail.com>
2022-09-07 23:23:22 +0200

    :partly_sunny: move clouds on scroll

--- src/Components/Games/background.svelte
+++ src/Components/Games/background.svelte
@@ -1,8 +1,7 @@
 <script>
-    let wave1 = document.getElementById("wave1");
-    let wave2 = document.getElementById("wave2");
-    let wave3 = document.getElementById("wave3");
-    let wave4 = document.getElementById("wave4");
+    import { onMount } from "svelte";
+
+    let wave1, wave2, wave3, wave4;
 
     window.addEventListener("scroll", function () {
         let value = window.scrollY;
@@ -11,6 +10,13 @@
         wave2.style.backgroundPositionX = 300 + value * -4 + "px";
         wave3.style.backgroundPositionX = 200 + value * 2 + "px";
         wave4.style.backgroundPositionX = 100 + value * -2 + "px";
+    });
+
+    onMount(() => {
+        wave1 = document.getElementById("wave1");
+        wave2 = document.getElementById("wave2");
+        wave3 = document.getElementById("wave3");
+        wave4 = document.getElementById("wave4");
     });
 </script>
 

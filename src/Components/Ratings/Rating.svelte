ca98728228f4ebb722bcd00ef36325d002c5ef41
cryptocashcashoo <cryptocashcashoo@gmail.com>
2022-09-08 11:32:18 +0200

    :fire: :hammer: Replace MovingContainer by svelte fly anim

--- src/Components/Ratings/Rating.svelte
+++ src/Components/Ratings/Rating.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-    import MovingContainer from "./MovingContainer.svelte";
     import RatingLetter from "./RatingLetter.svelte";
     import ActiveByScroll from "@/Components/ActiveByScroll.svelte";
     import type { RatingInfo } from "@/Database/ratings";
+    import { fly } from "svelte/transition";
+    import { getRandomNumber } from "@/Scripts/maths";
 
     type Directions = "U" | "R" | "L" | "D";
 
@@ -10,6 +11,8 @@
     export let startPercentage: number;
     export let endPercentage: number;
     export let mode: Directions;
+
+    let appearDuration = 340;
 
     let displayLetter: boolean = false;
     let setDisplayLetter = (value: boolean) => (displayLetter = value);
@@ -22,80 +25,94 @@
 </script>
 
 <ActiveByScroll {startPercentage} {endPercentage}>
-    <MovingContainer {startPercentage} {endPercentage} {mode} style="z-index:400">
-        {#if mode === "L"}
-            <div
-                class="columns"
-                on:mouseenter={() => setDisplayLetter(true)}
-                on:mouseleave={() => setDisplayLetter(false)}
-            >
-                <div class="ImageContainer">
-                    <img class="TcelaImage" src="/images/tcela_dibus/patotata.png" alt="Tcela speaking" />
-                </div>
-                <div class="LetterContainer" style="left:100%;">
-                    {#if displayLetter}
-                        <div class="MoveLR">
-                            <RatingLetter {ratingInfo} />
-                        </div>
-                    {/if}
-                </div>
+    {#if mode === "L"}
+        <div
+            class="Rating columns"
+            style="left: 0; top:{getRandomNumber(5, 70)}%"
+            transition:fly={{ x: -window.innerWidth / 2, duration: appearDuration }}
+            on:mouseenter={() => setDisplayLetter(true)}
+            on:mouseleave={() => setDisplayLetter(false)}
+        >
+            <div class="ImageContainer">
+                <img class="TcelaImage" src="/images/tcela_dibus/patotata.png" alt="Tcela speaking" />
             </div>
-        {:else if mode === "R"}
-            <div
-                class="columns"
-                on:mouseenter={() => setDisplayLetter(true)}
-                on:mouseleave={() => setDisplayLetter(false)}
-            >
-                <div class="LetterContainer" style="right:100%;">
-                    {#if displayLetter}
-                        <div class="MoveRL">
-                            <RatingLetter {ratingInfo} />
-                        </div>
-                    {/if}
-                </div>
-                <div class="ImageContainer">
-                    <img class="TcelaImage flipX" src="/images/tcela_dibus/patotata.png" alt="Tcela speaking" />
-                </div>
+            <div class="LetterContainer" style="left:100%;">
+                {#if displayLetter}
+                    <div transition:fly={{ x: -window.innerWidth / 2, duration: appearDuration }}>
+                        <RatingLetter {ratingInfo} />
+                    </div>
+                {/if}
             </div>
-        {:else if mode === "U"}
-            <div
-                class="rows"
-                on:mouseenter={() => setDisplayLetter(true)}
-                on:mouseleave={() => setDisplayLetter(false)}
-            >
-                <div class="ImageContainer">
-                    <img class="TcelaImage rotate90" src="/images/tcela_dibus/patotatav.png" alt="Tcela speaking" />
-                </div>
-                <div class="LetterContainer" style="top:100%;">
-                    {#if displayLetter}
-                        <div class="MoveUD">
-                            <RatingLetter {ratingInfo} />
-                        </div>
-                    {/if}
-                </div>
+        </div>
+    {:else if mode === "R"}
+        <div
+            class="Rating columns"
+            style="right: 0; top:{getRandomNumber(5, 70)}%"
+            transition:fly={{ x: window.innerWidth / 2, duration: appearDuration }}
+            on:mouseenter={() => setDisplayLetter(true)}
+            on:mouseleave={() => setDisplayLetter(false)}
+        >
+            <div class="LetterContainer" style="right:100%;">
+                {#if displayLetter}
+                    <div transition:fly={{ x: window.innerHeight / 2, duration: appearDuration }}>
+                        <RatingLetter {ratingInfo} />
+                    </div>
+                {/if}
             </div>
-        {:else if mode === "D"}
-            <div
-                class="rows"
-                on:mouseenter={() => setDisplayLetter(true)}
-                on:mouseleave={() => setDisplayLetter(false)}
-            >
-                <div class="LetterContainer" style="bottom:100%">
-                    {#if displayLetter}
-                        <div class="MoveDU">
-                            <RatingLetter {ratingInfo} />
-                        </div>
-                    {/if}
-                </div>
-                <div class="ImageContainer">
-                    <img class="TcelaImage flipY" src="/images/tcela_dibus/patotatav.png" alt="Tcela speaking" />
-                </div>
+            <div class="ImageContainer">
+                <img class="TcelaImage flipX" src="/images/tcela_dibus/patotata.png" alt="Tcela speaking" />
             </div>
-        {/if}
-    </MovingContainer>
+        </div>
+    {:else if mode === "U"}
+        <div
+            class="Rating rows"
+            style="top: 0; left:{getRandomNumber(15, 75)}%"
+            transition:fly={{ y: -window.innerHeight / 2, duration: appearDuration }}
+            on:mouseenter={() => setDisplayLetter(true)}
+            on:mouseleave={() => setDisplayLetter(false)}
+        >
+            <div class="ImageContainer">
+                <img class="TcelaImage rotate90" src="/images/tcela_dibus/patotatav.png" alt="Tcela speaking" />
+            </div>
+            <div class="LetterContainer" style="top:100%;">
+                {#if displayLetter}
+                    <div transition:fly={{ y: -window.innerHeight / 2, duration: appearDuration }}>
+                        <RatingLetter {ratingInfo} />
+                    </div>
+                {/if}
+            </div>
+        </div>
+    {:else if mode === "D"}
+        <div
+            class="Rating rows"
+            style="bottom: 0; right:{getRandomNumber(15, 75)}%"
+            transition:fly={{ y: window.innerHeight / 2, duration: appearDuration }}
+            on:mouseenter={() => setDisplayLetter(true)}
+            on:mouseleave={() => setDisplayLetter(false)}
+        >
+            <div class="LetterContainer" style="bottom:100%">
+                {#if displayLetter}
+                    <div transition:fly={{ y: window.innerHeight / 2, duration: appearDuration }}>
+                        <RatingLetter {ratingInfo} />
+                    </div>
+                {/if}
+            </div>
+            <div class="ImageContainer">
+                <img class="TcelaImage flipY" src="/images/tcela_dibus/patotatav.png" alt="Tcela speaking" />
+            </div>
+        </div>
+    {/if}
 </ActiveByScroll>
 
 <style>
+    .Rating {
+        position: fixed;
+        align-self: flex-start;
+        width: fit-content;
+        height: fit-content;
+        z-index: 400;
+    }
+
     .TcelaImage {
         width: 10vw;
         vertical-align: bottom;
@@ -114,71 +131,7 @@
         width: max-content;
     }
 
-    .ImageContainer {
-        z-index: 399;
-    }
-
     .ImageContainer:hover {
         animation: shake 250ms 2 linear;
-    }
-
-    .MoveUD {
-        animation: UD 500ms 1 ease-out;
-    }
-    @keyframes UD {
-        0% {
-            transform: translate(0, -100vh);
-        }
-        50% {
-            transform: translate(0, -100vh);
-        }
-        100% {
-            transform: translate(0, 0);
-        }
-    }
-
-    .MoveDU {
-        animation: DU 500ms 1 ease-out;
-    }
-    @keyframes DU {
-        0% {
-            transform: translate(0, 100vh);
-        }
-        50% {
-            transform: translate(0, 100vh);
-        }
-        100% {
-            transform: translate(0, 0);
-        }
-    }
-
-    .MoveLR {
-        animation: LR 500ms 1 ease-out;
-    }
-    @keyframes LR {
-        0% {
-            transform: translate(-100vw, 0);
-        }
-        50% {
-            transform: translate(-100vw, 0);
-        }
-        100% {
-            transform: translate(0, 0);
-        }
-    }
-
-    .MoveRL {
-        animation: RL 500ms 1 ease-out;
-    }
-    @keyframes RL {
-        0% {
-            transform: translate(100vw, 0);
-        }
-        50% {
-            transform: translate(100vw, 0);
-        }
-        100% {
-            transform: translate(0, 0);
-        }
     }
 </style>

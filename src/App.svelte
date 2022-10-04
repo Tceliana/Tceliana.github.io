acf31db8aaad37d0cc1467a4d11048be4687f2b5
Wilmi <83143288+CryptoCashCashoo@users.noreply.github.com>
2022-10-04 14:40:20 +0200

    Coauthored PR (#21)
    * Deleted comment on code
    
    * borrar salto de linea innecesario
    
    Co-authored-by: Carlos Moshu <carlos@carlosmoshu.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-    import { Router, Route } from "svelte-routing";
-
-    import Home from "@/Pages/Home.svelte";
-    import Header from "@/Components/Header/Header.svelte";
-    import Games from "@/Pages/Games.svelte";
+  import { Router, Route } from "svelte-routing";
+  import Home from "@/Pages/Home.svelte";
+  import Header from "@/Components/Header/Header.svelte";
+  import Games from "@/Pages/Games.svelte";
 </script>
 
 <main
-    style="
+  style="
 --COLOR_PRIMARY:            #fae035;
 --COLOR_SECONDARY:          #0064c8;
 --COLOR_TERTIARY:           #FFBC20;
@@ -16,11 +15,11 @@
 --COLOR_BUBBLE_BACKGROUND:  #4f4f4f;
 "
 >
-    <Header />
-    <Router>
-        <Route path="/" component={Home} />
-        <Route path="/games" component={Games} />
-    </Router>
+  <Header />
+  <Router>
+    <Route path="/" component={Home} />
+    <Route path="/games" component={Games} />
+  </Router>
 </main>
 
 <style>
